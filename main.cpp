0b3184b56358beb63b607c9066db1560f64af7ac
Mikołaj Lubiak <lubiak@proton.me>
2023-12-14 13:31:06 +0100

    Small refactor

--- main.cpp
+++ main.cpp
@@ -172,7 +172,6 @@ private:
 		vkDestroyCommandPool(device, commandPool, nullptr);
 		vkDestroyCommandPool(device, transferCommandPool, nullptr);
 
-
 		vkDestroyDevice(device, nullptr);
 
 		if (enableValidationLayers) {
@@ -193,7 +192,7 @@ private:
 		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
 		appInfo.pEngineName = "No Engine";
 		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-		appInfo.apiVersion = VK_API_VERSION_1_0;
+		appInfo.apiVersion = VK_API_VERSION_1_3;
 
 		VkInstanceCreateInfo createInfo{};
 		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
@@ -208,13 +207,14 @@ private:
 			throw std::runtime_error("not all requested validation layers are available");
 		}
 
-		VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
 		if (enableValidationLayers) {
 			createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
 			createInfo.ppEnabledLayerNames = validationLayers.data();
 
+			VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
+
 			populateDebugMessengerCreateInfo(debugCreateInfo);
-			createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*)&debugCreateInfo;
+			createInfo.pNext = &debugCreateInfo;
 		}
 		else {
 			createInfo.enabledLayerCount = 0;
@@ -269,7 +269,7 @@ private:
 		bool allNeededExtensionsSupported = true;
 		for (const char* extensionName : extensions) {
 			bool isExtensionSupported = false;
-			for (const auto& supportedExtension : supportedExtensions) {
+			for (const VkExtensionProperties& supportedExtension : supportedExtensions) {
 				if (strcmp(extensionName, supportedExtension.extensionName) == 0) {
 					isExtensionSupported = true;
 					break;
